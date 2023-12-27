522509c2724d6c4fc4eeea0701700e17d12ce2f9
Rakesh Kumar Sahoo <sahoorakeshkumar42@gmail.com>
2023-12-27 18:18:22 +0530

    add styling to heading

--- src/components/InsertData.jsx
+++ src/components/InsertData.jsx
@@ -4,7 +4,7 @@ const InsertData = () => {
 	return (
 		<div className='w-full mt-10 flex flex-col justify-center items-center'>
 			<div>
-				<h2 className='text-2xl uppercase '>Insert Data</h2>
+				<h2 className='text-2xl uppercase'>Insert Data</h2>
 			</div>
 
 			<div>
--- src/components/ViewAllData.jsx
+++ src/components/ViewAllData.jsx
@@ -3,7 +3,7 @@ import { BasicTable } from "./index";
 const ViewAllData = () => {
 	return (
 		<div className='flex flex-col justify-center items-center mt-10'>
-			<h2>All Users</h2>
+			<h2 className='text-2xl uppercase'>All Users</h2>
 
 			<div>
 				<BasicTable />
